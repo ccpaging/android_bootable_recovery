860c4bfcd026a016ef824270544f3092af7d7f62
Xuefer <xuefer@gmail.com>
2015-11-28 22:46:11 +0800

    allow pulling busybox even toolbox is used
    Change-Id: Iac2d79846b106c124313ab8aaa09f9184818b5f4

--- Android.mk
+++ Android.mk
@@ -506,7 +506,6 @@ LOCAL_POST_INSTALL_CMD := \
 
 include $(BUILD_PHONY_PACKAGE)
 
-ifneq ($(TW_USE_TOOLBOX), true)
 include $(CLEAR_VARS)
 # Create busybox symlinks... gzip and gunzip are excluded because those need to link to pigz instead
 BUSYBOX_LINKS := $(shell cat external/busybox/busybox-full.links)
@@ -546,7 +545,6 @@ ALL_DEFAULT_INSTALLED_MODULES += $(RECOVERY_BUSYBOX_SYMLINKS)
 endif
 include $(BUILD_PHONY_PACKAGE)
 RECOVERY_BUSYBOX_SYMLINKS :=
-endif # !TW_USE_TOOLBOX
 
 # recovery-persist (system partition dynamic executable run after /data mounts)
 # ===============================

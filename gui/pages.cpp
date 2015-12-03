16d9da70d740f7bc2db2ca432296e23218a27c28
Xuefer <xuefer@gmail.com>
2015-12-03 14:17:23 +0800

    restore current page on theme reload
    Change-Id: Ia16e21b3b5f651a2298625b2af490ac551954705

--- gui/pages.cpp
+++ gui/pages.cpp
@@ -939,6 +939,11 @@ int PageSet::SetPage(std::string page)
 	return -1;
 }
 
+std::string PageSet::GetPage() const
+{
+	return mCurrentPage ? mCurrentPage->GetName() : std::string();
+}
+
 int PageSet::SetOverlay(Page* page)
 {
 	if (page) {
@@ -1496,6 +1501,10 @@ int PageManager::ReloadPackage(std::string name, std::string package)
 	PageSet* set = (*iter).second;
 	mPageSets.erase(iter);
 
+	std::string currentPage;
+	if (mCurrentSet)
+		currentPage = mCurrentSet->GetPage();
+
 	if (LoadPackage(name, package, mStartPage) != 0)
 	{
 		LOGINFO("Failed to load package '%s'.\n", package.c_str());
@@ -1505,6 +1514,10 @@ int PageManager::ReloadPackage(std::string name, std::string package)
 	if (mCurrentSet == set)
 		SelectPackage(name);
 	delete set;
+
+	if (!currentPage.empty())
+		mCurrentSet->SetPage(currentPage);
+
 	GUIConsole::Translate_Now();
 	return 0;
 }
